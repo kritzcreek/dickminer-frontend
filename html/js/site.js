6a9b425b1e6b1f77baa40b218f7b398d5d07c4e6
Christoph Hegemann <christoph.hegemann@gmx.net>
2015-07-10 18:12:27 +0000

    Initial Commit

--- html/js/site.js
+++ html/js/site.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     var tagline = document.querySelector("p.tagline");
-    tagline.innerText = "From Cloud9 IDE!";
+    tagline.innerText = "Welcome to Dickminer!";
+    //document.getElementById('testButton').onclick = getRoot;
 });
+
+/*
+* Maps the button click to the root route
+*/
+function getRoot(){
+    var r = new XMLHttpRequest();
+    r.open("GET", "/city", true);
+    r.onreadystatechange = function () {
+      if (r.readyState != 4 || r.status != 200) return;
+      alert("Success: " + r.responseText);
+    };
+    r.send("banana=yellow");
+}
--- node.js/server.js
+++ node.js/server.js
@@ -1,14 +1,19 @@
-// reference the http module so we can create a webserver
-var http = require("http");
+var express = require('express');
+var app = express();
 
-// create a server
-http.createServer(function(req, res) {
-    // on every request, we'll output 'Hello world'
-    res.end("Hello world from Cloud9!");
-}).listen(process.env.PORT, process.env.IP);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-// Note: when spawning a server on Cloud9 IDE, 
-// listen on the process.env.PORT and process.env.IP environment variables
+app.get('/city', function (req, res) {
+  res.send('CITY GOT HIT!');
+});
 
-// Click the 'Run' button at the top to start your server,
-// then click the URL that is emitted to the Output tab of the console
+app.use(express.static('../html'));
+
+var server = app.listen(process.env.PORT, process.env.IP, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
